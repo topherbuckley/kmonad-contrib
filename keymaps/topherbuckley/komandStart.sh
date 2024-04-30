f1d09a9b0fae09884172df73fd4b9c3e2e824009
Christopher Buckley <topherbuckley@gmail.com>
2024-04-30 17:16:24 +0900

    Updating scripts to allow choice between usb and wifi keyboard

--- keymaps/topherbuckley/komandStart.sh
+++ keymaps/topherbuckley/komandStart.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-/home/bo/.local/bin/kmonad /media/christopher/HDD/git/kmonad-contrib/keymaps/topherbuckley/config.kbd
+KBD_DEV=$(find /dev/input/by-id/*kbd* | fzf)
+export KBD_DEV
+KBDCFG=$(envsubst < "/media/christopher/HDD/git/kmonad-contrib/keymaps/topherbuckley/config.kbd")
+
+echo "KBD_DEV: $KBD_DEV"
+echo "KBDCFG: $KBDCFG"
+
+kmonad <(echo "$KBDCFG") > /dev/null
