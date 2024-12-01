ccc50e85286288db46d60935d2d92d877ba007e4
Christopher Buckley <topherbuckley@gmail.com>
2024-12-01 13:59:48 +0900

    Updating local dir

--- keymaps/topherbuckley/komandStart.sh
+++ keymaps/topherbuckley/komandStart.sh
@@ -2,7 +2,7 @@
 
 KBD_DEV=$(find /dev/input/by-id/*kbd* | fzf)
 export KBD_DEV
-KBDCFG=$(envsubst < "/media/christopher/HDD/git/kmonad-contrib/keymaps/topherbuckley/config.kbd")
+KBDCFG=$(envsubst < "/home/bo/code/kmonad-contrib/keymaps/topherbuckley/config.kbd")
 
 echo "KBD_DEV: $KBD_DEV"
 echo "KBDCFG: $KBDCFG"
