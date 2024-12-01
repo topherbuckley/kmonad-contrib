e3883ba1b440420a1a57655facb90dacf5322371
Christopher Buckley <topherbuckley@gmail.com>
2024-12-01 15:08:35 +0900

    Updating to rigid device on usb

--- keymaps/topherbuckley/komandStart.sh
+++ keymaps/topherbuckley/komandStart.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-KBD_DEV=$(find /dev/input/by-id/*kbd* | fzf)
-export KBD_DEV
 KBDCFG=$(envsubst < "/home/bo/code/kmonad-contrib/keymaps/topherbuckley/config.kbd")
 
-echo "KBD_DEV: $KBD_DEV"
 echo "KBDCFG: $KBDCFG"
 
 kmonad <(echo "$KBDCFG") > /dev/null
